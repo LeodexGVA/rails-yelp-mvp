45697dc5eb76e5a6dde1e501914122ee59fcc6cf
Dylan Cianci <cianci.dy@gmail.com>
2023-08-23 16:52:22 +0200

    exercice 2

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  resources :restaurants, only: [ :new, :create, :show, :index ] do
+    resources :reviews, only: [:new, :create, :index]
+  end
+  root "restaurants#index"
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+Restaurant.destroy_all
+
+restaurants = Restaurant.create([
+  { name: 'Restaurant 1', address: 'Address 1', phone_number: '022 500 50 50', category: 'chinese' },
+  { name: 'Restaurant 2', address: 'Address 2', phone_number: '123 456 789', category: 'italian' },
+  { name: 'Restaurant 3', address: 'Address 3', phone_number: '987 654 321', category: 'japanese' },
+  { name: 'Restaurant 4', address: 'Address 4', phone_number: '555 555 555', category: 'french' },
+  { name: 'Restaurant 5', address: 'Address 5', phone_number: '111 222 333', category: 'belgian' }
+  ])
